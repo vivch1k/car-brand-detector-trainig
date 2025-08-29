eadfffdf3f43c3091f5fdfad68146bf9f2d22d7c
vova <vovam207@gmail.com>
2025-08-29 11:59:45 +0300

    Create create_dataloaders function

--- src/data_setup.py
+++ src/data_setup.py
@@ -1,16 +1,37 @@
-import torch
 import torchvision
 from torch.utils.data import DataLoader 
-from torchvision import transforms, datasets
-from transformers import AutoImageProcessor
+from torchvision import datasets
 
-def collate_fn():
-    pass
 
 def create_dataloaders(train_path: str,
                        test_path: str,
-                       transform: torchvision.transforms,
+                       train_transform: torchvision.transforms,
+                       test_transform: torchvision.transforms,
                        batch_size: int,
                        num_worker: int=0,
-                       pin_memory: bool=False):
-    pass
+                       pin_memory: bool=False,
+                       collate_fn: function=None):
+    
+    train_data = datasets.ImageFolder(root=train_path,
+                                      transform=train_transform)
+    
+    test_data = datasets.ImageFolder(root=test_path,
+                                    transform=test_transform)
+    
+    class_names = train_data.classes
+    
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  num_workers=num_worker,
+                                  pin_memory=pin_memory,
+                                  collate_fn=collate_fn)
+    
+    test_dataloader = DataLoader(dataset=test_data,
+                                 batch_size=batch_size,
+                                 shuffle=False,
+                                 num_workers=num_worker,
+                                 pin_memory=pin_memory,
+                                 collate_fn=collate_fn)
+    
+    return train_dataloader, test_dataloader, class_names
