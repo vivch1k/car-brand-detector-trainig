b249c275fff4135de1d9f88bf0a127992ff37584
vova <vovam207@gmail.com>
2025-08-30 20:18:02 +0300

    add plot_curves function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,28 @@
 import torch
 import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def set_seeds(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     random.seed(seed)
+    
+    
+def plot_curves(results: dict):
+    range_epochs = list(range(len(results["train_loss"])))
+      
+    plt.figure(figsize=(12, 5))
+    
+    plt.subplot(1, 2, 1)
+    sns.lineplot(x=range_epochs, y=results["train_loss"], label="train_loss", color="red")
+    sns.lineplot(x=range_epochs, y=results["test_loss"], label="test_loss", color="blue")
+    plt.grid(True)
+    
+    plt.subplot(1, 2, 2)
+    sns.lineplot(x=range_epochs, y=results["train_acc"], label="train_acc", color="red")
+    sns.lineplot(x=range_epochs, y=results["test_acc"], label="test_acc", color="blue")
+    plt.grid(True)
+    
+    plt.savefig("data/artifacts/training_curves.png")
